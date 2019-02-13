698af7f1164cae901b45883f3af0d5f25d12b6a4
Alex Babenko <bob78alex@gmail.com>
2019-02-12 19:54:59 -0500

    Adding API call

--- rjq.js
+++ rjq.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+	getQuote();
 var randomQuote, randomNum, randomAuthor;
+var text, author;
 function getQuote(){
-
+/* 
 	var quotes = ["Hello World", "Hey, I'm here!", "Whats up"];
 	var authors = ["Author 1", "Author 2", "Author 3"];
 
@@ -10,15 +12,22 @@ function getQuote(){
 	randomAuthor = authors[randomNum];
 
 	$(".quote").text(randomQuote);
-	$(".author").text(randomAuthor);
-	
+	$(".author").text(randomAuthor);*/
+	var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+	$.getJSON(url, function(data){
+$(".quote").html('"' + data.quoteText + '"');
+$(".author").html('- ' + data.quoteAuthor);
+text = data.quoteText;
+author = data.quoteAuthor;
+	});
+
 }
 $("#newQuote").on("click", function(){
 getQuote();
 });
 
 $("#tweet").on("click", function(){
-	window.open("https://twitter.com/intent/tweet?text="+"'"+randomQuote+"'" +" - "+ randomAuthor);
+	window.open("https://twitter.com/intent/tweet?text="+"'"+text+"'" +" - "+ author);
 });
 
 	
